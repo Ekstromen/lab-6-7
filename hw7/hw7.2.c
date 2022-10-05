64bb8b4dd45e71c117f936d99276df45bb14954e
Ekstromen <113119290+Ekstromen@users.noreply.github.com>
2022-10-05 20:30:26 +0300

    Update hw7.2.c

--- hw7/hw7.2.c
+++ hw7/hw7.2.c
@@ -8,7 +8,7 @@ int main()
 	float x, y;
 	char z;
 	setlocale(LC_ALL, "rus");
-	printf("Введите пример вводя каждый символ отдельно: \n");
+	printf("Р’РІРµРґРёС‚Рµ РїСЂРёРјРµСЂ РІРІРѕРґСЏ РєР°Р¶РґС‹Р№ СЃРёРјРІРѕР» РѕС‚РґРµР»СЊРЅРѕ: \n");
 	scanf("%f%c%f",&x,&z,&y);
 	switch (z)
 	{
@@ -24,7 +24,7 @@ int main()
 	case '/':
 		printf("%f / %f = %.2f",x , y, x/y);
 		break;
-	default: printf("Команда не распознана");
+	default: printf("РљРѕРјР°РЅРґР° РЅРµ СЂР°СЃРїРѕР·РЅР°РЅР°");
 		break;
 	}
 
