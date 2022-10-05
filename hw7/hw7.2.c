8cce628022833f7774c10ec732eac1db69e4d53d
Ekstromen <113119290+Ekstromen@users.noreply.github.com>
2022-10-05 20:29:59 +0300

    Add files via upload

---
+++ hw7/hw7.2.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	float x, y;
+	char z;
+	setlocale(LC_ALL, "rus");
+	printf("Введите пример вводя каждый символ отдельно: \n");
+	scanf("%f%c%f",&x,&z,&y);
+	switch (z)
+	{
+	case '+':
+		printf("%f + %f = %f", x, y, x+y);
+		break;
+	case '-':
+		printf("%f - %f = %f", x, y, x-y);
+		break;
+	case '*':
+		printf("%f * %f = %f", x, y, x*y);
+		break;
+	case '/':
+		printf("%f / %f = %.2f",x , y, x/y);
+		break;
+	default: printf("Команда не распознана");
+		break;
+	}
+
+}
